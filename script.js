b3389dceda54853df2511a72b9e47589522eec7b
rxlxtxvxst <chrisbillones07@gmail.com>
2025-02-07 21:04:08 +0800

    script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let video = document.getElementById("intro-video");
 
     video.addEventListener("ended", function () {
-        video.style.display = "none"; // Hides video after it finishes
+        video.style.display = "none";
     });
 
-    // Ensure iPad support for autoplay
+   
     video.play().catch(() => {
         video.muted = true;
         video.play();
